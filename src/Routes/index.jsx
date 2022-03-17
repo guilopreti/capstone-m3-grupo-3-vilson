abcf6c5feaea623ad884bffbbef76f7bd9b0843c
Igo Santana <igo.santana33@gmail.com>
2022-03-17 11:26:56 -0300

    feat: Alteração nas pastas

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,33 +1,30 @@
-import { Switch, Route } from "react-router-dom";
-import Register from "../pages/Register/index";
-// import Login from "../pages/login/index";
-import Login from "../pages/Login/index";
+import { Switch, Route } from 'react-router-dom'
+import Register from '../pages/Register/index'
+import Login from '../pages/Login/index'
+import ArticlePage from '../pages/ArticlePage'
+import Home from '../pages/Home'
+import User from '../pages/User'
 
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/">
-        {/* Rota inicial com os cards de noticias */}
-        Home
+      <Route exact path='/'>
+        <Home />
       </Route>
-      <Route path={"/register"}>
-        {/* Rota de Cadastro */}
+      <Route path={'/register'}>
         <Register />
       </Route>
-      <Route path={"/login"}>
-        {/* Rota de Login */}
+      <Route path={'/login'}>
         <Login />
       </Route>
-      <Route path={"/user"}>
-        User
-        {/* Rota mostrando perfil de um user com suas noticias */}
+      <Route path={'/user'}>
+        <User />
       </Route>
-      <Route path={"/post"}>
-        Post
-        {/* Rota mostrando uma noticia */}
+      <Route exact path='/post'>
+        <ArticlePage />
       </Route>
     </Switch>
-  );
-};
+  )
+}
 
-export default Routes;
+export default Routes
