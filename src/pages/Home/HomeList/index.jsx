53fe5ea9a6a1bf073261c65723f7c0f1c32fb9ac
Guilherme <guilherme.llopreti@gmail.com>
2022-03-23 17:33:08 -0300

    fix: renderização dos posts a serem revisados

--- src/pages/Home/HomeList/index.jsx
+++ src/pages/Home/HomeList/index.jsx
@@ -1,8 +1,12 @@
 import MediumCard from "../../../components/CardNews/MediumCard";
 import HomeListContainer from "./styled";
 import { postsDb } from "../../../Providers/posts/postsDb";
+import { PostsContext } from "../../../Providers/posts/index.js";
+import { useContext } from "react";
 
 const HomeList = () => {
+  const { posts } = useContext(PostsContext);
+
   return (
     <HomeListContainer>
       <ul>
