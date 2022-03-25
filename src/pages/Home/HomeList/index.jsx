80dcfb37708a4348b17602fe4e338ad989044d5a
Guilherme <guilherme.llopreti@gmail.com>
2022-03-24 23:08:37 -0300

    feature: titulos na home e espaçamento de botões

--- src/pages/Home/HomeList/index.jsx
+++ src/pages/Home/HomeList/index.jsx
@@ -8,7 +8,6 @@ import {
   HomeListTitle,
   HomeListTitleTop,
 } from "./styled";
-import { postsDb, postsDb2, postsDb3 } from "../../../Providers/posts/postsDb";
 import { PostsContext } from "../../../Providers/posts/index.js";
 import { useContext } from "react";
 
@@ -21,51 +20,24 @@ const HomeList = ({ inputValue }) => {
 
   return (
     <>
-      <HomeListTitleTop>
-        <h2>Tecnologia</h2>
-      </HomeListTitleTop>
-      <HomeListContainerTop>
-        <ul>
-          {postsDb2.map((current) => (
-            <MediumCard key={current.id} current={current} />
-          ))}
-        </ul>
-      </HomeListContainerTop>
-      <HomeListTitle>
-        <h2>Mundo</h2>
-      </HomeListTitle>
-      <HomeListPrimaryContainer>
-        <ul className="primary-ul-bigCard">
-          {postsDb.map((current) => (
-            <BigCard key={current.id} current={current} />
-          ))}
-        </ul>
-        <ul className="primary-ul-smallCard">
-          {postsDb2.map((current) => (
-            <SmallCard key={current.id} current={current} />
-          ))}
-        </ul>
-      </HomeListPrimaryContainer>
-      <HomeListTitle>
-        <h2>Artigos</h2>
-      </HomeListTitle>
-      <HomeListContainer>
-        <ul>
-          {postsDb3.map((current) => (
-            <SmallCard key={current.id} current={current} />
-          ))}
-        </ul>
-      </HomeListContainer>
       {postsDbFiltered.length > 0 ? (
-        <HomeListContainerTop>
-          <ul>
-            {postsDbFiltered.map((current) => {
-              return <MediumCard key={current.id} current={current} />;
-            })}
-          </ul>
-        </HomeListContainerTop>
+        <>
+          <HomeListTitleTop>
+            <h2>{inputValue}</h2>
+          </HomeListTitleTop>
+          <HomeListContainerTop>
+            <ul>
+              {postsDbFiltered.map((current) => {
+                return <MediumCard key={current.id} current={current} />;
+              })}
+            </ul>
+          </HomeListContainerTop>
+        </>
       ) : (
         <>
+          <HomeListTitleTop>
+            <h2>Tecnologia</h2>
+          </HomeListTitleTop>
           <HomeListContainerTop>
             <ul>
               {posts.map((current) => {
@@ -75,6 +47,9 @@ const HomeList = ({ inputValue }) => {
               })}
             </ul>
           </HomeListContainerTop>
+          <HomeListTitle>
+            <h2>Mundo</h2>
+          </HomeListTitle>
           <HomeListPrimaryContainer>
             <ul className="primary-ul-bigCard">
               {posts.map((current) => {
@@ -91,6 +66,9 @@ const HomeList = ({ inputValue }) => {
               })}
             </ul>
           </HomeListPrimaryContainer>
+          <HomeListTitle>
+            <h2>Artigos</h2>
+          </HomeListTitle>
           <HomeListContainer>
             <ul>
               {posts.map((current) => {
