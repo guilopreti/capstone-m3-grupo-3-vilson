02e1e7a4ffaf9c1f4a3fff993f6e64160065b1d6
Guilherme Lopreti <guilherme.llopreti@gmail.com>
2023-06-06 23:38:46 -0300

    fix: api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://apifake-capstone-m3.herokuapp.com",
+  baseURL: "https://opnion-fakeapi.onrender.com",
 });
 
 export default api;
